bb9e93710f0aed3cf582ef6e5a35b90b24dfd8c4
Eldad <eldadf456@gmail.com>
2024-03-10 13:01:38 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,25 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
     def save(self):
         """ save updated """
         self.updated_at = datetime.utcnow()
+
     def to_dict(self):
         """    """
         inst_dict = self.__dict__.copy()
@@ -22,6 +33,7 @@ class BaseModel:
         inst_dict["updated_at"] = self.updated_at.isoformat()
 
         return inst_dict
+
     def __str__(self):
         """   """
         class_name = self.__class__.__name__
