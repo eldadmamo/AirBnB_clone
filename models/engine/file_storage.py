a80cc2f8c3a5355eba79e905059c05029512488e
Eldad <eldadf456@gmail.com>
2024-03-10 19:22:19 -0700

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """
-Module data
+Module for serializing and deserializing data
 """
 import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+
 
 class FileStorage:
     """
-    FileStorage class data
+    FileStorage class for storing, serializing and deserializing data
     """
     __file_path = "file.json"
 
@@ -17,19 +23,28 @@ class FileStorage:
 
     def new(self, obj):
         """
-
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
         """
         obj_cls_name = obj.__class__.__name__
+
         key = "{}.{}".format(obj_cls_name, obj.id)
+
         FileStorage.__objects[key] = obj
+
+
     def all(self):
         """
-        Returns dictionary
+        Returns the __objects dictionary. 
+        It provides access to all the stored objects.
         """
         return  FileStorage.__objects
+
+
     def save(self):
         """
-
+        Serializes the __objects dictionary into 
+        JSON format and saves it to the file specified by __file_path.
         """
         all_objs = FileStorage.__objects
 
@@ -37,11 +52,13 @@ class FileStorage:
 
         for obj in all_objs.keys():
             obj_dict[obj] = all_objs[obj].to_dict()
+
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(obj_dict, file)
+
     def reload(self):
         """
-        JSON file
+        This method deserializes the JSON file
         """
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
